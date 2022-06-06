30bfdbeb681f11fcc299a19db997e9a4754301c5
DaviMattos14 <davimattos2012@gmail.com>
2022-06-06 17:45:46 -0300

    Questão 1

--- P1_AED/Questão_1/Questao1.r
+++ P1_AED/Questão_1/Questao1.r
@@ -1,53 +1,37 @@
 library(ISLR)
 attach(College)
 
-names(College)
-head(College)
-dim(College)
-
 #Criando a imagendo do BoxPlot
-jpeg("S.F.Ratio.jpeg")
-boxplot(S.F.Ratio, main = "Número de Estudantes por Professor",
+jpeg("S.F.Ratio_Tr.jpeg")
+boxplot(round(S.F.Ratio), main = "Número de Estudantes por Professor",
+# boxplot(((S.F.Ratio^-2)-1)/(-2))
 xlab = "Professor",
 ylab = "Estudantes",
+yaxp = c(0,40,8),
 col = "#7f7fdb"
 )
 dev.off()
 
-#Transformando a variável
-simetr <- function(lambda){
-    tr <- ((S.F.Ratio^lambda)-1)/lambda
-    q1 <- quantile(tr,0.25)
-    q2 <- quantile(tr,0.5)
-    q3 <- quantile(tr,0.75)
-    return(abs(q2-0.5*(q1+q3))/q2)
-}
-aux <- c(-2,-1,-0.5,0.5,1,2)
-for (num in aux) {
-    print(num)
-    print(simetr(num))
-}
-teste <- 999
-lbd <- 0
-for(lambda in aux){
-    if (simetr(lambda) < teste) {
-       teste <- simetr(lambda)
-       lbd <- lambda
-    }
-}
-print(lbd)
-print(teste)
-boxplot(((S.F.Ratio^lbd)-1)/lbd)
-boxplot(S.F.Ratio)
-
 # Analisando os Dados e Calculando os Cercas e Diferença Interquartil
-sort(S.F.Ratio)
 summary(S.F.Ratio)
 quantile(S.F.Ratio)
-
 DIQ <- 16.5-11.5
 inf <- 11.5- 1.5 * DIQ
-sup <- 16.5 + 1.5 * DIQ
+sup <- 15.5 + 1.5 * DIQ
 
-S.F.Ratio
+#Filtrando o nome da Universidade com S.F.Ratio
+df_College <- data.frame(name_College=rep(rownames(College)), ratio=rep(round(S.F.Ratio)))
 
+#df_College <- data.frame(name_College=rep(rownames(College)), ratio=rep(S.F.Ratio))
+
+filtro1 <- subset(College, round(S.F.Ratio) < inf, select=c(S.F.Ratio)) # Outlines Inferiores
+filtro2 <- subset(College, round(S.F.Ratio) > sup, select=c(S.F.Ratio)) # Outlines Superiores
+
+filtro3 <- subset(College, S.F.Ratio < 4, select=c(S.F.Ratio))
+filtro4 <- subset(College, S.F.Ratio > 24, select=c(S.F.Ratio))
+
+View(df_College)
+boxplot(S.F.Ratio)
+boxplot(round(S.F.Ratio))
+View(filtro1)
+View(filtro2)
