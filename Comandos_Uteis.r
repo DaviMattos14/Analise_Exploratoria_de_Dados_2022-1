ea86cbef25da395c69fab4c0ba5f850950c1a7b1
DaviMattos14 <davimattos2012@gmail.com>
2022-07-17 18:19:02 -0300

    P2_Q2

--- Comandos_Uteis.r
+++ Comandos_Uteis.r
@@ -9,7 +9,80 @@ boxplot(S.F.Ratio) #Boxplot
 hist(S.F.Ratio) #Histogrma
 plot(S.F.Ratio) #Scatterplot
 summary(S.F.Ratio)# resumos de várias funções de ajuste de modelo
- 
+
+help(sqrt) # help(comando) para saber mais sobre algum comando
+shell("cls") # Comando para Limpar o cmd
+q() #Comando para Sair do R
+
+# Atribuindo Valores
+x <- 5
+0.54 -> y
+z = 2 #não recomendável
+assign("a",6)
+
+# Vetores
+x <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+sum(x) # Soma todos os valores do vetor
+prod(x) # Multiplica todos os valores do vetor
+max(x) # Pega o maior número do vetor
+min(x) # Pega o menor Número do Vetor
+
+# vetor de "a" até "z"
+seq(from= a, to= z)
+#vetor de "a" até "z" com passo "n"
+seq(from= a, to= z, by= n )
+#vetor de "a" até "z" com "n" elementos
+seq(from= a, to= z, length.out= n)
+
+# Matrizes
+matriz <- matrix(valor, num_linha, num_coluna) # matrix(data = NA, nrow = x, ncol = y, byrow = TRUE | T | F | FALSE)
+rbind(vetor1, vetor2, ..., vetorN)
+cbind(vetor1, vetor2, ..., vetorN)
+m <- matrix(x,2,5)
+
+# Operações Matemáticas
+a=2; b=3; c=5 
+a*b
+b-c
+a+c
+b^a
+
+# Operações Lógicas
+a==b
+b<c
+c!=a
+
+# GRÁFICOSS
+
+a<-c(0.12, 0.3, 0.26, 0.16, 0.04, 0.12)
+names(a)<-c("a","b","c","d","e","f")
+pie(a,col = c("red","blue","green","gray", "brown", "black")) #Gráfico de Pizza
+
+#--------------------------------------------------------------------------------------------------------------
+# --------------------------------------- ESTATÍSCA BÁSICA ---------------------------------------------------- 
+#--------------------------------------------------------------------------------------------------------------
+
+x <- c(10, 14, 13, 15, 16, 18, 12)
+mean(x) # Média Aritimética
+
+median(x) # Mediana
+
+# Moda
+y <- c(7,8,9,10,10,10,11,12)
+table(y)
+subset(table(y),table(y)==max(table(y)))
+
+choose(n,k) # Combinatória
+
+factorial(x) #Fatorial
+
+# Média Ponderada
+notas <- c(4, 2, 8)
+pesos <- c(1,1,2)
+sum((notas*pesos))/sum(pesos)
+
+
 #Criando .jpeg do Gráfico
 jpeg("S.F.Ratio.jpeg")
 boxplot(S.F.Ratio)
---
+++ P2_AED/Questão 2/Q2.r
@@ -0,0 +1,79 @@
+cells <- c(73.77574, 81.28325, 94.07271, 104.92151, 118.88436, 86.69515, 95.23071, 109.83399, 127.11574, 148.41316, 86.68249, 98.59934, 112.64397, 123.08970, 136.94859)
+
+novos_valores <- c()
+for(i in 1:length(cells)){
+    novos_valores <- c(novos_valores, log(cells[i]))
+}
+
+rnames <- c("Ate34","35a49","50+")
+cnames <- c("1","2","3","4","5")
+tabela_wage <- matrix(novos_valores,3,5,byrow = TRUE,dimnames=list(rnames,cnames))
+tabela_wage
+
+d <- dim(tabela_wage)
+
+yhat <- c()
+delta <- matrix(0,3,5)
+for(j in 1:5){
+    delta[,j] <- tabela_wage[,j] - median(tabela_wage[,j]) # matriz delta
+    yhat <- c(yhat, median(tabela_wage[,j])) # yhat
+}
+
+centr <- median(yhat) #Valor central
+
+rowef <- c()
+for(i in 1:3){
+    rowef <- c(rowef, median(delta[i,])) # efeito de linha
+}
+
+colef <- numeric(5)
+for (j in 1:5) {
+   colef[j] <- yhat[j] - centr # efeito de coluna
+}
+
+res <- matrix(0,3,5) #Erro
+for(i in 1:3){
+    res[i,] <- delta[i,] - median(delta[i,])
+}
+
+comparacao_valores <- matrix(0,3,5)
+for(i in 1:3){
+    for(j in 1:5){
+        comparacao_valores[i,j] <- (rowef[i]*colef[j])/centr
+    }
+}
+
+cv<-numeric(length(rnames)*length(cnames))
+sr<-numeric(length(rnames)*length(cnames))
+for (i in 1:length(rnames)) {
+   for (j in 1:length(cnames)) {
+      cv[(i-1)*5+j] <- comparacao_valores[i,j]
+      sr[(i-1)*5+j] <- res[i,j]
+   }
+}
+
+ls.fit=lm(sr~cv)
+ls.fit
+
+incl=sr/cv
+incli=na.omit(incl)
+median(incli)
+
+tabela_wage
+
+x <- c(-1,-1,1,1)
+y <- c(0,0.5,0.5,1)
+
+jpeg("grafico_wage_age.jpeg")
+plot(x,y)
+
+abline(4.778151,1)
+abline(4.528275,1)
+abline(4.309985,1)
+abline(4.017729,1)
+abline(3.823909,1)
+
+abline(4.778151,-1)
+abline(5.221849,-1)
+abline(5.061061,-1)
+dev.off()
